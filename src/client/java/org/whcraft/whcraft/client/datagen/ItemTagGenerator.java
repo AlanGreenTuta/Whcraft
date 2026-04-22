4bd21f7df99418f9aa989748c9a93cacfeb945cc
42 <AlanGreen@tuta.io>
2026-04-22 21:39:17 +0800

    Update ItemTagGenerator.java

--- src/client/java/org/whcraft/whcraft/client/datagen/ItemTagGenerator.java
+++ src/client/java/org/whcraft/whcraft/client/datagen/ItemTagGenerator.java
@@ -1,3 +1,18 @@
+/**
+ * WhCraft - A Minecraft Mod about Warhammer 40,000.
+ * Copyright (C) 2026 ShuShu, 42
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.whcraft.whcraft.client.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
